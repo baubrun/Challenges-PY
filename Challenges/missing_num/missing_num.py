06c02158961bf97865f2d143517676d22e65b346
baubrun <baubelf@gmail.com>
2020-09-07 20:21:18 -0400

    updated challenge

--- Challenges/missing_num/missing_num.py
+++ Challenges/missing_num/missing_num.py
@@ -23,6 +23,3 @@ def missing_num(lst):
             break
     return 10 if ans is 0 else ans
 
-print(missing_num([1, 2, 3, 4, 6, 7, 8, 9, 10]) )
-print(missing_num([7, 2, 3, 6, 5, 9, 1, 4, 8]) )
-print(missing_num([10, 5, 1, 2, 4, 6, 8, 3, 9]) )
